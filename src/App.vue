8c2bfc9c926aff7942043d32d7d92a13f49b5bf6
Carol B <carolina.blanco.7e6@itb.cat>
2023-10-12 12:36:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import{ref, type Ref} from 'vue'
+const name:string="vars reactives";
+let counter:Ref<number>=ref(0);
+const increment = ()=>{
+  counter.value++;
+  console.log(counter);
+  
+};
+
+const decrement = ()=>{
+  counter.value--;
+  console.log(counter);
+  
+};
+
+const reset = ()=>{
+  counter.value=0
+
+};
+</script>
+<template>
+  <h1>Hola {{ name }}</h1>
+  <h2>{{ counter }}</h2>
+  <button @click="increment">Botó increment</button>
+  <button @click="decrement">Botón decremento</button>
+  <button @click="reset">Reset</button>
+</template>
+
+
